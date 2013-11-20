09f97f31c96689d5140a211f9f1dd8554240d14b
iamtrask <liamtrask@gmail.com>
2013-11-20 01:39:24 -0600

    implemented NArrayDouble resizing.

--- src/org/api/hyperdrive/NArrayDouble.java
+++ src/org/api/hyperdrive/NArrayDouble.java
@@ -28,45 +28,59 @@
 
 package org.api.hyperdrive;
 
-public final class NArrayDouble extends NArray<Double>
-{
-  public NArrayDouble(int[] dimensions)
-  {
-    super(dimensions);
-    this.data = new double[super.size()];
-  }
+public final class NArrayDouble extends NArray<Double> {
+    public NArrayDouble(int[] dimensions) {
+        super(dimensions);
+        this.data = new double[super.size()];
+    }
 
-  @Override
-  public final Double get(int idx)
-  {
-    return data[idx];
-  }
+    @Override
+    public final Double get(int idx) {
+        return data[idx];
+    }
 
-  @Override
-  public final void set(int idx,Double value)
-  {
-    data[idx] = value;
-  }
+    @Override
+    public final void set(int idx, Double value) {
+        data[idx] = value;
+    }
 
-  public final double getDouble(int idx)
-  {
-    return data[idx];
-  }
+    public final double getDouble(int idx) {
+        return data[idx];
+    }
 
-  public final void setDouble(int idx,double value)
-  {
-    data[idx] = value;
-  }
+    public final void setDouble(int idx, double value) {
+        data[idx] = value;
+    }
 
-  public final double getDouble(int[] coords)
-  {
-    return data[super.indexOf(coords)];
-  }
+    public final double getDouble(int[] coords) {
+        return data[super.indexOf(coords)];
+    }
 
-  public final void setDouble(int[] coords,double value)
-  {
-    data[super.indexOf(coords)] = value;
-  }
+    public final void setDouble(int[] coords, double value) {
+        data[super.indexOf(coords)] = value;
+    }
 
-  private final double[] data;
+    public final NArrayDouble getResized(int[] newDim) throws Exception {
+
+        NArrayDouble output = new NArrayDouble(newDim);
+
+        if (this.size() == output.size()) {
+            int[] coordsOld = new int[dimensions().length];
+            int[] coordsNew = new int[newDim.length];
+
+            for (int i = 0, j = this.size(); i < j; ++i) {
+
+                output.setDouble(coordsNew, this.get(coordsOld));
+
+                Coord.advance(coordsOld, dimensions());
+                Coord.advance(coordsNew, newDim);
+
+            }
+            return output;
+        } else {
+            throw new Exception("Can't resize to a matrix with a different number of values.");
+        }
+    }
+
+    private final double[] data;
 }
